10cce2d497d09383972ce0ab98c741e4b5fd9d98
xjanua <tonybo@live.unc.edu>
2014-11-02 22:15:36 -0500

    helloworld

--- app/js/snake.js
+++ app/js/snake.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	ctx.canvas.height = window.innerHeight;
 	var w = $("#canvas").width();
 	var h = $("#canvas").height();
-	var word = "fuckyouu";
+	var word = "helloworld";
 	var token = word.split("");
 	var remaining_letters_in_array;
 	var letter_location_in_array = 0;
